4a88e79f25ca5a501faa369de44933afe0d178d1
Petr Losev <geraltrivski@mail.ru>
2024-09-17 14:57:50 +0300

    TheFooter component

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,19 @@
+<template>
+  <footer>
+    <p>all rights reserved</p>
+  </footer>
+</template>
+
+<style lang="scss">
+@import '@/assets/styles/_mixins';
+@import '@/assets/styles/_variables';
+
+footer {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 1.875rem 0;
+  background-color: getcolor('grey.lighten2');
+  font-size: 0.813rem;
+}
+</style>
--- src/views/ViewForm.vue
+++ src/views/ViewForm.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
 import TheHeader from '@/components/TheHeader.vue'
+import TheFooter from '@/components/TheFooter.vue';
 </script>
 
 <template>
   <TheHeader />
+
+  <TheFooter />
 </template>
